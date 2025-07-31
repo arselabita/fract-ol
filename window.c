e24f4b56bdc711e4e9132beca1b56e72ab1ba2bb
Arsela Bita <arselabita@gmail.com>
2025-07-31 13:04:49 +0200

    this is ok, maybe add smth later for hook part

--- window.c
+++ window.c
@@ -24,10 +24,8 @@ static int ft_exit(t_data *data)
 void    my_pixel_put(t_img img, int x, int y, int color)
 {
     char* dest;
-
-    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+    if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT || !img.addr)
         return;
-    
     dest = img.addr + (y * img.linelen + x * (img.bpp / 8));
     *(unsigned int*)dest = color;
 }
@@ -37,37 +35,15 @@ static int keyhandler(int key, t_data *data)
         ft_exit(data);
     return (0);
 }
-void start_loop(t_data *data)
+void init_window_and_display(t_data *data, t_fractal *fract)
 {
+    data->mlx = mlx_init();
+    data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "The Fract-ol"); 
+    data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT); 
+    data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bpp, \
+									  &data->img.linelen, &data->img.endian);
+    ft_mandelbrot(fract);
     mlx_hook(data->win, 2, 1L << 0, keyhandler, data);
     mlx_hook(data->win, 17, 1L << 2, ft_exit, data);
     mlx_loop(data->mlx);
-}
-void init_window_and_display(t_data *data)
-{
-    data->mlx = mlx_init();
-    if (!data->mlx)
-    {
-        write(2, "mlx_init failed\n", 16);
-        exit(1);
-    }
-    data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "The Fract-ol"); 
-    if (!data->win)
-    {
-        write(2, "mlx_new_window failed\n", 23);
-        exit(1);
-    }
-    data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT); 
-    if (!data->img.img)
-    {
-        write(2, "mlx_new_image failed\n", 22);
-        exit(1);
-    }
-    data->img.addr = mlx_get_data_addr(data->img.img, &data->img.bpp, \
-									  &data->img.linelen, &data->img.endian);
-    if (!data->img.addr)
-    {
-        write(2, "mlx_get_data_addr failed\n", 26);
-        exit(1);
-    }
 }
