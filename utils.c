e92fd15ea20aff5983f028bbe5f1d342ab3e79f0
Arsela Bita <arselabita@gmail.com>
2025-08-07 21:02:38 +0200

    a function with helper funct

---
+++ utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 20:57:48 by abita             #+#    #+#             */
+/*   Updated: 2025/08/07 20:57:52 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+double magnitude(t_complex z)
+{
+    return (sqrt(z.real * z.real + z.imag * z.imag));
+}
+t_complex complex_square(t_complex z)
+{
+    t_complex result;
+ 
+    result.real = (z.real * z.real) - (z.imag * z.imag);
+    result.imag = 2 * z.real * z.imag;
+    return (result);
+}
+void color_range(t_fractal *fract, t_color renk)
+{
+    if (fract->base_color & 0xFF0000)
+        renk.r = fract->intensity;
+    else if (fract->base_color & 0x00FF00)
+        renk.g = fract->intensity;
+    else if (fract->base_color & 0x0000FF)
+        renk.b = fract->intensity;        
+}
+void ft_color_fract(t_fractal *fract, t_color renk, int i)
+{
+    if (i == fract->max_iter)
+        fract->color = BLACK;
+    else
+    {
+        fract->intensity = ((i + 1) * 255 / fract->max_iter) * fract->base_color;
+        color_range(fract, renk);
+        fract->color = (renk.r << 16) | (renk.g >> 8) | renk.b;
+    }
+}
